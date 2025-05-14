8860b8e791e5f73d7863dd366f582eaefd0457d0
PINNINTI BHARADWAJ <166684495+Bharadwajpinninti@users.noreply.github.com>
2025-05-14 18:34:03 +0530

    Create ProductCart.jsx

---
+++ ProductCart.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+function ProductCard({ title, price, image, rating }) {
+
+
+    console.log(rating)
+    return (
+        <div className="card">
+            <img src={image} />
+            <p>{title}</p>
+            <p> ${price}</p>
+            <p>{rating.rate}</p>
+
+        </div>
+
+    )
+}
+
+export default ProductCard
